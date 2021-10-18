b232f84a2889641b1039331aabc09ceecb85bca3
caowei190214 <1102596646@qq.com>
2021-10-18 16:25:40 +0800

    feat:添加了注册功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,27 +1,52 @@
 <template>
-  <div style="width: 100%; height: 100vh; overflow: hidden"> <!--  :style="bg" 加背景图片-->
+  <div style="width: 100%; height: 100vh; overflow: hidden">
+    <!--  :style="bg" 加背景图片-->
     <div style="width: 400px; margin: 100px auto">
-      <div style="font-size: 30px; text-align: center; padding: 30px 0">欢迎登录</div>
+      <div style="font-size: 30px; text-align: center; padding: 30px 0">
+        欢迎登录
+      </div>
       <el-form ref="form" :model="form" size="normal" :rules="rules">
         <el-form-item prop="username">
-          <el-input prefix-icon="el-icon-user-solid" v-model="form.username"></el-input>
+          <el-input
+            prefix-icon="el-icon-user-solid"
+            v-model="form.username"
+          ></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input prefix-icon="el-icon-lock" v-model="form.password" show-password></el-input>
+          <el-input
+            prefix-icon="el-icon-lock"
+            v-model="form.password"
+            show-password
+          ></el-input>
         </el-form-item>
-        <el-form-item>
-          
-        </el-form-item>
+        <el-form-item> </el-form-item>
         <el-row>
           <el-col :span="10">
             <el-form-item>
-            <el-button style="width: 100%" type="primary" @click="login">登 录</el-button>
+              <el-button style="width: 100%" type="primary" @click="login"
+                >登 录</el-button
+              >
             </el-form-item>
           </el-col>
           <el-col :span="10">
-            <el-form-item>
-            <el-button style="width: 100%" type="primary" @click="login">注册</el-button>
-            </el-form-item>
+            <el-button style="width: 100%" @click="dialogVisible = true"
+              >注册</el-button
+            >
+
+            <el-dialog
+              title="注册"
+              :visible.sync="dialogVisible"
+              width="30%"
+              :before-close="handleClose"
+            >
+              <Register></Register>
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">立即注册</el-button>
+                <el-button type="primary" @click="dialogVisible = false"
+                  >确 定</el-button
+                >
+              </span>
+            </el-dialog>
           </el-col>
         </el-row>
       </el-form>
@@ -30,41 +55,41 @@
 </template>
 
 <script>
-
+import Register from "./Register.vue";
 export default {
-  name: 'Login',
-  data () {
+  components: { Register },
+  name: "Login",
+  data() {
     return {
-      form: {role: 1},
+      form: { role: 1 },
+      dialogVisible: false,
       rules: {
         username: [
-          {required: true, message: '请输入用户名', trigger: 'blur'}
+          { required: true, message: "请输入用户名", trigger: "blur" },
         ],
-        password: [
-          {required: true, message: '请输入密码', trigger: 'blur'}
-        ]
-      }
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
       // 加背景图片
       // bg: {
       //   backgroundImage: "url(" + require("@/assets/bg.jpg") + ")",
       //   backgroundRepeat: "no-repeat",
       //   backgroundSize: "100% 100%"
       // }
-    }
+    };
   },
-  created () {
-    sessionStorage.removeItem('user')
+  created() {
+    sessionStorage.removeItem("user");
   },
   methods: {
-    login () {
-      this.$refs['form'].validate((valid) => {
-        if (valid) {}
-      })
-    }
-  }
-}
+    login() {
+      this.$refs["form"].validate((valid) => {
+        if (valid) {
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/views/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          prefix-icon="el-icon-lock"
+          v-model="form.password1"
+          show-password
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="请再输入">
+        <el-input
+          prefix-icon="el-icon-lock"
+          v-model="form.password2"
+          show-password
+        ></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "register",
+  data() {
+    return {
+      form: {
+        username: "",
+        password1: "",
+        password2: "",
+      },
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    onsubmit() {},
+  },
+};
+</script>
+
+<style>
+</style>
